72a28e09c2353418f557afc962107334d209e480
koopercavender <koopcavender@gmail.com>
2023-05-17 23:41:07 -0500

    updated url

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://rose-weak-piranha.cyclic.app/api/${plantName}`)
+        const response = await fetch(`https://rose-weak-piranha.cyclic.app/api/${playerName}`)
         const data = await response.json()
 
         console.log(data)
